dca2034aa0b4dcc00a68d0967247f6078f83b23f
Rohit Gangshettiwar <omkar.patil@media4u.com>
2023-09-06 16:02:41 +0530

    Assessment 8

---
+++ Assessment_8/Assessment_8.js
@@ -0,0 +1,24 @@
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    // let temp=calcTip(bills[i]);
+    tips.push(calcTip(bills[i]));
+    totals.push(tips[i] + bills[i]);
+}
+console.log(tips, totals);
+
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+console.log(calcAverage(totals));
