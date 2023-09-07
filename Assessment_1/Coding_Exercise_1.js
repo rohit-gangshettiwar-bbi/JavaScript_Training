179ccbc99814e3e34b149e0d0bf3d65623439b50
Rohit Gangshettiwar <rohit.gangshettiwar@media4u.com>
2023-09-07 18:20:21 +0530

    JS Assessment

--- Assessment_1/Coding_Exercise_1.js
+++ Assessment_1/Coding_Exercise_1.js
@@ -1,14 +1,11 @@
-const massMark=78;
-const heightMark=1.69;
-const massJohn=92;
-const heightJohn=1.95;
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
 
- 
-let BMIJohn=massJohn/(heightJohn*heightJohn);
-let BMIMark=massMark/(heightMark*heightMark);
-console.log(BMIMark,BMIJohn);
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+let BMIMark = massMark / (heightMark * heightMark);
+console.log(BMIMark, BMIJohn);
 
-
-const markHigherBMI= BMIMark>BMIJohn;
-console.log(markHigherBMI,"Mark has higher BMI than John");
-
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI, "Mark has higher BMI than John");
